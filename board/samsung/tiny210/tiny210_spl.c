b4870c3318a9490adce6652453c1b21f1f22a321
Mike Dyer <mike.dyer@md-soft.co.uk>
2012-06-05 23:51:38 +0100

    Remove SPL stuff that was too big Don't reinit when u-boot executes in RAM

--- board/samsung/tiny210/tiny210_spl.c
+++ board/samsung/tiny210/tiny210_spl.c
@@ -26,87 +26,18 @@
 #include <asm/arch/mmc.h>
 #include <asm/arch/cpu.h>
 
+typedef u32(*copy_sd_mmc_to_mem)
+(u32 channel, u32 start_block, u16 block_size, u32 *trg, u32 init);
 
-#include <fat.h>
-#include <malloc.h>
-#include <mmc.h>
-
-DECLARE_GLOBAL_DATA_PTR;
-
-/* Define global data structure pointer to it*/
-static gd_t gdata __attribute__ ((section(".data")));
-static bd_t bdata __attribute__ ((section(".data")));
-unsigned int s5p_cpu_id;
-
-void __udelay(unsigned long delay)
-{
-	unsigned int loops;
-	for (;delay > 0; delay--)
-		for (loops = 250; loops > 0; loops --)
-			asm ("nop");
-}
-
-void board_init_f(unsigned long bootflag)
-{
-	/*
-	 * We call relocate_code() with relocation target same as the
-	 * CONFIG_SYS_SPL_TEXT_BASE. This will result in relocation getting
-	 * skipped. Instead, only .bss initialization will happen. That's
-	 * all we need
-	 */
-	serial_puts(">>board_init_f()\n");
-	relocate_code(CONFIG_SYS_INIT_SP_ADDR & ~7, &gdata, CONFIG_SPL_TEXT_BASE);
-	while(1);
-}
-
-void hang (void){
-	serial_puts("Board Hung...\n");
-	while(1);
-}
-
-/* Place Holders */
-void board_init_r(gd_t *id, ulong dest_addr)
-{
-	struct mmc *mmc;
-	int err, i;
-	serial_puts(">>board_init_r()\n");
-
-	unsigned char *hdr;
-
-	gd = &gdata;
-	gd->bd = &bdata;
-	gd->have_console = 1;
-	gd->flags = 0;
-
-	s5p_set_cpu_id();
-	printf("CPU is 0x%0x\n", s5p_cpu_id);
-
-	mem_malloc_init(CONFIG_SYS_SPL_MALLOC_START, CONFIG_SYS_SPL_MALLOC_SIZE);
-
-	mmc_initialize(gd->bd);
-
-	mmc = find_mmc_device(0);
-	if (!mmc) {
-		puts("spl: mmc device not found!!\n");
-		hang();
+void __spin(unsigned long d) {
+	int loops;
+	for (; d > 0; d --) {
+		for (loops = 500; loops > 0; loops --)
+			asm("nop");
 	}
-
-	err = mmc_init(mmc);
-	if (err) {
-		printf("spl: mmc init failed: err - %d\n", err);
-		hang();
-	}
-
-	hdr = malloc(512);
-	err = mmc->block_dev.block_read(0,
-			33, 1,
-			(void *)hdr);
-
-	hang();
 }
 
-
-void __serial_puts(const char *s) {
+void serial_puts(const char *s) {
 	while (*s != 0) {
 		serial_putc(*s++);
 	}
@@ -119,46 +50,65 @@ void serial_putc(const char c)
 		*txh = '\r';
 	*txh = c;
 }
+
+void copy_mmc_to_ram(unsigned int blk_offset, unsigned int blk_count, void *destination)
+{
+	ulong ch;
+	ch = *(volatile u32 *)(0xD0037488);
+	copy_sd_mmc_to_mem copy_bl2 =
+	    (copy_sd_mmc_to_mem) (*(u32 *) (0xD0037F98));
+
+	u32 ret;
+	if (ch == 0xEB000000) {
+		ret = copy_bl2(0, blk_offset, blk_count, destination, 0);
+	}
+	else if (ch == 0xEB200000) {
+		ret = copy_bl2(2, blk_offset, blk_count, destination, 0);
+	}
+}
+
+void board_init_f(unsigned long bootflag)
+{
+        __attribute__((noreturn)) void (*uboot)(void);
+
+	unsigned int *inform3 = (unsigned int *)0xE010F00C;
+
+	unsigned char *dest = (unsigned char *)CONFIG_SYS_TEXT_BASE;
+	int i;
+
+	serial_puts("Copying u-boot to ram\n");
+	copy_mmc_to_ram(CONFIG_SPL_UBOOT_OFS, CONFIG_SPL_UBOOT_SIZ, dest);
+
+        /* Jump to U-Boot image */
+	*inform3 = 0x0000bead; /* Don't bother to reinit */
+        uboot = (void *)CONFIG_SYS_TEXT_BASE;
+        (*uboot)();
+
+	while(1);
+}
+
+/* Place Holders */
+void board_init_r(gd_t *id, ulong dest_addr)
+{
+	while(1);
+}
+
+
+void hex_char(unsigned char x) {
+
+	serial_puts("0x");
+
+	if ( (x & 0xF0) >= 0xa0)
+		serial_putc(( (x >> 4) & 0xF) - 0xa + 'A');
+	else
+		serial_putc(( (x >> 4) & 0xF) + '0');
+
+	if ( (x & 0xF) >= 0xa)
+		serial_putc((x & 0xF) - 0xa + 'A');
+	else
+		serial_putc((x & 0xF) + '0');
 	
-void serial_puts(const char *s) {
 }
 
-#ifdef CONFIG_GENERIC_MMC
-void set_mmc_pins(int ch)
-{
-	unsigned int *gpio = (unsigned int *)S5PC110_GPIO_BASE;
-	if (ch == 0) {
-		gpio[0x1a0 / 4] = 0x02222222;	/* MMC	    */
-		gpio[0x1a8 / 4] = 0x0;		/* No Pull  */
-		gpio[0x1ac / 4] = 0x3FFF;	/* 4X Drive */
-		printf("gpg0: 0x%08x 0x%08x 0x%08x\n", gpio[0x1a0 / 4], gpio[0x1a8 / 4], gpio[0x1ac / 4]);
-	}
 
-	if (ch == 2) {
-		gpio[0x1e0 / 4] = 0x02222222;	/* MMC	    */
-		gpio[0x1e8 / 4] = 0x0;		/* No Pull  */
-		gpio[0x1ec / 4] = 0x3FFF;	/* 4X Drive */
-	}
-}
-
-int board_mmc_init(bd_t *bis)
-{
-	ulong ch = *(volatile u32 *)(0xD0037488);
-	printf("MMC @ 0x%08x\n", (unsigned int)ch);
-
-	if (ch == 0xeb000000UL) {
-		set_mmc_pins(0);
-		s5p_mmc_init(0, 4);
-
-	} else if (ch == 0xeb200000UL) {
-		set_mmc_pins(2);
-		s5p_mmc_init(2, 4);
-	} else {
-		printf("No MMC!\n");
-		return -1;
-	}
-
-	return 0;
-}
-#endif
 
